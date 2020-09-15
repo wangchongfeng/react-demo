68ba9749b40d38f046f997f33ce6493835f909e2
wangchongfeng <m15011384464@163.com>
2020-09-15 17:03:14 +0800

    [wangcf] #n/a add hooks demo

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import './App.css';
 import Myform from './pages/Myform/index.js'
 import Redux from './pages/redux'
+import useCallbackPage from './pages/useCallback'
+import useMemoPage from './pages/useMemo'
 import "antd/dist/antd.css"
-// import { BrowserRouter as Router, Link, Route, Redirect } from "react-router-dom";
-import { BrowserRouter as Router, Link, Route, Redirect } from "./components/react-router-dom";
+import { BrowserRouter as Router, Link, Route, Redirect } from "react-router-dom";
+// import { BrowserRouter as Router, Link, Route, Redirect } from "./components/react-router-dom";
 
 function App() {
   return (
@@ -12,10 +14,14 @@ function App() {
       <Router >
         <Link to="/form">表单</Link>
         <Link to="/redux">Redux</Link>
+        <Link to="/useMemo">useMemoPage</Link>
+        <Link to="/useCallback">useCallbackPage</Link>
         {/* <Redirect to="/form" /> */}
-        <Route  path="/" component={Myform} />
+        {/* <Route  path="/" component={Myform} /> */}
         <Route  path="/form" component={Myform} />
         <Route  path="/redux" component={Redux} />
+        <Route  path="/useMemo" component={useMemoPage} />
+        <Route  path="/useCallback" component={useCallbackPage} />
       </Router>
     </div>
   );
